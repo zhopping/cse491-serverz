2357ebe41a59d15afb33e96c06f3f0948e40b001
C. Titus Brown <titus@idyll.org>
2014-01-07 17:12:47 -0500

    initial commit of server.py

---
+++ server.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import random
+import socket
+import time
+
+s = socket.socket()         # Create a socket object
+host = socket.gethostname() # Get local machine name
+port = random.randint(8000, 9999)
+s.bind((host, port))        # Bind to the port
+
+print 'Starting server on', host, port
+print 'The Web server URL for this would be http://%s:%d/' % (host, port)
+
+s.listen(5)                 # Now wait for client connection.
+
+print 'Entering infinite loop; hit CTRL-C to exit'
+while True:
+    # Establish connection with client.    
+    c, (client_host, client_port) = s.accept()
+    print 'Got connection from', client_host, client_port
+    c.send('Thank you for connecting')
+    c.send("good bye.")
+    c.close()
