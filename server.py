469bc9c132b5b9c626ba9cae12b1e2aae921e8db
xavier.durandhollis <xavierdhjr@gmail.com>
2014-01-12 16:00:39 -0500

    Updated server.py to display a hello world message. Server now prints out Request headers from client on the console.

--- server.py
+++ server.py
@@ -15,9 +15,31 @@ s.listen(5)                 # Now wait for client connection.
 
 print 'Entering infinite loop; hit CTRL-C to exit'
 while True:
-    # Establish connection with client.    
-    c, (client_host, client_port) = s.accept()
-    print 'Got connection from', client_host, client_port
-    c.send('Thank you for connecting')
-    c.send("good bye.")
-    c.close()
+	# Establish connection with client.    
+	c, (client_host, client_port) = s.accept()
+	print c.recv(1000)
+	#HTTP/1.1 200 OK
+	#Content-Type: text/html; charset=utf-8
+
+	print 'Got connection from', client_host, client_port
+
+
+	response_body_raw = "<html><body><h1>Hello world!</h1>This is xavierdhjr's Web server.</body></html>"
+	
+	response_headers = {
+		'Content-Type': 'text/html; encoding=utf8',
+		'Content-Length': len(response_body_raw),
+		'Connection': 'close'
+	}
+
+	response_headers_raw = ''.join('%s: %s \r\n' % (k, v) for k, v in response_headers.iteritems())
+	
+	c.send('HTTP/1.1 200 OK \r\n')
+#	c.send('Content-Type: text/html; encoding=utf8 \r\n')
+#	c.send('Content-Length: %d\r\n' % (len(response_body_raw)))
+#	c.send('Connection: close\r\n')
+	c.send(response_headers_raw)
+	c.send('\r\n') # to separate headers from body
+	c.send(response_body_raw)
+
+	c.close()
