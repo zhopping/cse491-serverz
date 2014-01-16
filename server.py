37da703f3829097ddd9de7050799e3ffb47cdacb
Kenneth Massie <massiek1@arctic.cse.msu.edu>
2014-01-15 21:43:40 -0500

    Modified server.py to return HTTP 1.0 response

--- server.py
+++ server.py
@@ -7,10 +7,8 @@ s = socket.socket()         # Create a socket object
 host = socket.getfqdn() # Get local machine name
 port = random.randint(8000, 9999)
 s.bind((host, port))        # Bind to the port
-
 print 'Starting server on', host, port
 print 'The Web server URL for this would be http://%s:%d/' % (host, port)
-
 s.listen(5)                 # Now wait for client connection.
 
 print 'Entering infinite loop; hit CTRL-C to exit'
@@ -18,6 +16,7 @@ while True:
     # Establish connection with client.    
     c, (client_host, client_port) = s.accept()
     print 'Got connection from', client_host, client_port
-    c.send('Thank you for connecting')
-    c.send("good bye.")
+    c.send('HTTP/1.0 200 OK\n')
+    c.send('Content-Type:text/html\n\n')
+    c.send("<h1>Hello, world</h1> this is massiek's Web server.")
     c.close()
