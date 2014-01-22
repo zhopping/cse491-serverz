cadea5faef169e1359c2b1650b69f00ff97eea97
Zack Hopping <zachary.hopping@gmail.com>
2014-01-22 12:43:26 -0500

    added main and handle_connection functions in server.py

--- server.py
+++ server.py
@@ -3,25 +3,30 @@ import random
 import socket
 import time
 
-s = socket.socket()         # Create a socket object
-host = socket.getfqdn() # Get local machine name
-port = random.randint(8000, 9999)
-s.bind((host, port))        # Bind to the port
+def main():
+    s = socket.socket()         # Create a socket object
+    host = socket.getfqdn() # Get local machine name
+    port = random.randint(8000, 9999)
+    s.bind((host, port))        # Bind to the port
 
-print 'Starting server on', host, port
-print 'The Web server URL for this would be http://%s:%d/' % (host, port)
+    print 'Starting server on', host, port
+    print 'The Web server URL for this would be http://%s:%d/' % (host, port)
 
-s.listen(5)                 # Now wait for client connection.
+    s.listen(5)                 # Now wait for client connection.
 
-print 'Entering infinite loop; hit CTRL-C to exit'
-while True:
-    # Establish connection with client.    
-    c, (client_host, client_port) = s.accept()
-    c.send("HTTP/1.0 200 OK\r\n")
-    c.send("Content-type:text/html\r\n")
-    c.send("\r\n") # Separate headers from body
-    c.send("<h1>Hello world</h1> this is zhopping's Web server\r\n")
+    print 'Entering infinite loop; hit CTRL-C to exit'
+    while True:
+        # Establish connection with client.    
+        c, (client_host, client_port) = s.accept()
+def handle_connection(conn):
+    conn.send("HTTP/1.0 200 OK\r\n")
+    conn.send("Content-type:text/html\r\n")
+    conn.send("\r\n") # Separate headers from body
+    conn.send("<h1>Hello world</h1> this is zhopping's Web server\r\n")
     print 'Got connection from', client_host, client_port
-    c.send('Thank you for connecting')
-    c.send("good bye.")
-    c.close()
+    conn.send('Thank you for connecting')
+    conn.send("good bye.")
+    conn.close()
+
+if __name__ == '__main__':
+    main()
