d71f3a50a04f591742133bd8f6ed3799ea3155e3
xavier.durandhollis <xavierdhjr@gmail.com>
2014-01-22 19:29:55 -0500

    hw2 merge

--- server.py
+++ server.py
@@ -3,21 +3,40 @@ import random
 import socket
 import time
 
-s = socket.socket()         # Create a socket object
-host = socket.getfqdn() # Get local machine name
-port = random.randint(8000, 9999)
-s.bind((host, port))        # Bind to the port
+def main():
+	s = socket.socket()         # Create a socket object
+	host = socket.getfqdn() # Get local machine name
+	port = random.randint(8000, 9999)
+	s.bind((host, port))        # Bind to the port
 
-print 'Starting server on', host, port
-print 'The Web server URL for this would be http://%s:%d/' % (host, port)
+	print 'Starting server on', host, port
+	print 'The Web server URL for this would be http://%s:%d/' % (host, port)
 
-s.listen(5)                 # Now wait for client connection.
+	s.listen(5)                 # Now wait for client connection.
 
-print 'Entering infinite loop; hit CTRL-C to exit'
-while True:
-    # Establish connection with client.    
-    c, (client_host, client_port) = s.accept()
-    print 'Got connection from', client_host, client_port
-    c.send('Thank you for connecting')
-    c.send("good bye.")
-    c.close()
+	print 'Entering infinite loop; hit CTRL-C to exit'
+	while True:
+		# Establish connection with client.    
+		c, (client_host, client_port) = s.accept()
+		print c.recv(1000)
+		print 'Got connection from', client_host, client_port
+		c.send('Thank you for connecting')
+		response_body_raw = "<html><body><h1>Hello world!</h1>This is xavierdhjr's Web server.</body></html>"
+		
+		response_headers = {
+			'Content-Type': 'text/html; encoding=utf8',
+			'Content-Length': len(response_body_raw),
+			'Connection': 'close'
+		}
+		response_headers_raw = ''.join('%s: %s \r\n' % (k, v) for k, v in response_headers.iteritems())
+		
+		c.send('HTTP/1.1 200 OK \r\n')
+		c.send(response_headers_raw)
+		c.send(response_body_raw)
+		c.close()
+
+def handle_connection(conn):
+	pass
+
+if __name__ == '__main__':
+   main()
